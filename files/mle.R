eeffe9dc3113ee3b969296b295cd2265359d104e
acarril <acarril@fen.uchile.cl>
2016-11-06 23:30:56 -0300

    update r code

--- files/mle.R
+++ files/mle.R
@@ -1,11 +1,11 @@
 # The true parameters
-beta <- 1.5
-sigma2 <- 0.5
+beta <- 2
+sigma2 <- 1
 
 # Generate dataset
-data   <- data.frame(x = runif(300, 1, 10))
-data$y <- 0 + 0.5*beta*data$x + rnorm(300, 0, sigma2)
-plot(data$x, data$y)
+data   <- data.frame(x = runif(100, 1, 10))
+data$y <- beta*data$x + rnorm(100, 0, sigma2)
+plot(data$x, data$y, xlab="x", ylab="y")
 
 # Log-likelihood function
 linear.lik <- function(theta, y, X){
@@ -14,15 +14,15 @@ linear.lik <- function(theta, y, X){
   beta   <- theta[1:k]
   sigma2 <- theta[k+1]
   e      <- y - X%*%beta
-  logl   <- -.5*n*log(2*pi)-.5*n*log(sigma2) - ( (t(e) %*% e)/ (2*sigma2) )
+  logl   <- -.5*n*log(2*pi) - .5*n*log(sigma2) - ((t(e) %*% e) / (2*sigma2))
   return(-logl)
 }
 
 # Plot of log-likelihood function for different values of beta and sigma2
 surface <- list()
 k <- 0
-for(beta in seq(0, 2, 0.1)){
-  for(sigma2 in seq(0, 2, 0.1)){
+for(beta in seq(0, 3, 0.1)){
+  for(sigma2 in seq(0, 3, 0.1)){
     k <- k + 1
     logL <- linear.lik(theta = c(0, beta, sigma2), y = data$y, X = cbind(1, data$x))
     surface[[k]] <- data.frame(beta = beta, sigma2 = sigma2, logL = -logL)
@@ -32,16 +32,14 @@ surface <- do.call(rbind, surface)
 library(lattice)
 wireframe(logL ~ beta*sigma2,
           surface,
-          colorkey=T,
+          colorkey=FALSE,
           drape=TRUE,
-          scales = list(arrows = FALSE, z = list(arrows=T)),
-          col.regions = rainbow(100, s = 1, v = 1, start = 0, end = max(1,100 - 1)/100, alpha = 1)
-          trellis.par.set(background = list(col = "transparent"))
-          )
+          scales = list(arrows = FALSE, z = list(arrows=TRUE)),
+          col.regions = rainbow(100, s = 1, v = 1, start = 0, end = max(1,100 - 1)/100, alpha = 1))
 
+# Find maximization points
 linear.MLE <- nlm(f=linear.lik, p=c(1,1,1), hessian=TRUE, y=data$y, X=cbind(1, data$x))
 linear.MLE$estimate
-
-## [1] -0.04481  2.70841  1.71870
+## [1] -0.4578486  2.0923226  0.7413104
 
 summary(lm(y ~ x, data))
